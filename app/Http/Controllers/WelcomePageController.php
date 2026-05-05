698151fcc5a2813b5578afe5f37d19be8b101979
LabibArefin123 <ah947777@gmail.com>
2026-05-06 00:34:16 +0600

    service page removed

--- app/Http/Controllers/WelcomePageController.php
+++ app/Http/Controllers/WelcomePageController.php
@@ -62,31 +62,6 @@ class WelcomePageController extends Controller
         return view('frontend.service_page.service', compact('services'));
     }
 
-    public function service_page_1()
-    {
-        return view('frontend.service_page.page_1.service_1');
-    }
-
-    public function service_page_2()
-    {
-        return view('frontend.service_page.page_2.service_2');
-    }
-
-    public function service_page_3()
-    {
-        return view('frontend.service_page.page_3.service_3');
-    }
-
-    public function service_page_4()
-    {
-        return view('frontend.service_page.page_4.service_4');
-    }
-
-    public function service_page_5()
-    {
-        return view('frontend.service_page.page_5.service_5');
-    }
-
     public function appointment()
     {
         $doctorAppointments = Appointment::with('doctor')
--- resources/views/frontend/custom_layout/footer.blade.php
+++ resources/views/frontend/custom_layout/footer.blade.php
@@ -21,11 +21,11 @@
             <div class="col-md-3">
                 <h6>Services</h6>
                 <ul>
-                    <li><a href="{{ route('service_1') }}">Full Body Health Checkup</a></li>
-                    <li><a href="{{ route('service_2') }}">X-Ray Scan</a></li>
-                    <li><a href="{{ route('service_3') }}">Blood Pressure Check</a></li>
-                    <li><a href="{{ route('service_4') }}">Blood Sugar Test</a></li>
-                    <li><a href="{{ route('service_5') }}">Full Blood Count (CBC)</a></li>
+                    <li><a href="{{ route('service.show', 1) }}">Full Body Health Checkup</a></li>
+                    <li><a href="{{ route('service.show', 2) }}">X-Ray Scan</a></li>
+                    <li><a href="{{ route('service.show', 3) }}">Blood Pressure Check</a></li>
+                    <li><a href="{{ route('service.show', 4) }}">Blood Sugar Test</a></li>
+                    <li><a href="{{ route('service.show', 5) }}">Full Blood Count (CBC)</a></li>
                 </ul>
             </div>
 
