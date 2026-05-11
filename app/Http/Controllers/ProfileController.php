b291086eb86083b36c13b4b19c36e54d90111ddd
LabibArefin123 <ah947777@gmail.com>
2026-05-11 20:28:24 +0600

    dashboard page default page fixed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,19 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Http\Requests\ProfileUpdateRequest;
-use App\Models\Role;
-use App\Models\Organization;
-use App\Models\OrganizationEnlistment;
-use App\Models\OrganizationDocument;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
